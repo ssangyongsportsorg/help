a52c85a6ea5efefe670b2d39e8129d0748bbd3f7
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-08-28 16:31:08 +0800

    Update ExploreContent.js

--- src/components/ExploreContent/ExploreContent.js
+++ src/components/ExploreContent/ExploreContent.js
@@ -5,7 +5,67 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
 
 const firstDataRow = [
-title: "References",
+  {
+    title: "Get Started",
+    icon: "img/get_started-icon.png",
+    link: "stable/getting-started/overview",
+    description: (
+      <>
+        Learn about Zowe™ architecture, components, and how to quickly get
+        started with Zowe. Read about what's new and changed in the Release
+        Notes, FAQs.
+      </>
+    ),
+  },
+  {
+    title: "Setup and Use",
+    icon: "img/user_guide-icon.png",
+    link: "stable/user-guide/install-overview",
+    description: (
+      <>
+        Find out how to install and configure Zowe. Learn about how to use 
+        different Zowe components and a numer of Zowe applications and plug-ins.
+      </>
+    ),
+  },
+  {
+    title: "Extend",
+    icon: "img/extend-icon.png",
+    link: "stable/extend/extend-zowe-overview",
+    description: (
+      <>
+        Learn about onboarding your products. Developers can follow tutorials
+        that teach how to build and extend Zowe components.
+      </>
+    ),
+  },
+];
+
+const secondDataRow = [
+  {
+    title: "Troubleshoot",
+    icon: "img/troubleshooting-icon.png",
+    link: "stable/troubleshoot/troubleshooting",
+    description: (
+      <>
+        Get troubleshooting tips to help you understand and effectively resolve
+        problems with Zowe.
+      </>
+    ),
+  },
+  {
+    title: "Contribute",
+    icon: "img/contribute-icon.png",
+    link: "stable/contribute/roadmap-contribute",
+    description: (
+      <>
+        Learn about how you can contribute to Zowe and this documentation. Read about 
+        the Zowe UI and code guidelines.
+      </>
+    ),
+  },
+  {
+    title: "References",
     icon: "img/references-icon.png",
     link: "stable/appendix/zowe-cli-command-reference",
     description: (
@@ -79,5 +139,4 @@ function Features() {
   );
 }
 
-export default Features; 
-            
+export default Features;
